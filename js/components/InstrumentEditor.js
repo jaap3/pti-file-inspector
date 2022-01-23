603318940d549330dbb7bc8d4696ea476928020b
Jaap Roes <jaap.roes@gmail.com>
2022-01-23 18:08:50 +0100

    Dry up slider activation code

--- js/components/InstrumentEditor.js
+++ js/components/InstrumentEditor.js
@@ -16,6 +16,32 @@ function getTemplate({ ownerDocument }) {
   return templateCache.get(ownerDocument)
 }
 
+function activateSlider(input, headerData, { defaultValue = 0, formatValue = (value) => value } = {}) {
+  const output = input.form[`${input.name}-result`]
+
+  function showValue() {
+    input.value = headerData[input.name]
+    output.value = formatValue(headerData[input.name])
+  }
+
+  input.addEventListener('wheel', (e) => {
+    headerData[input.name] = Math.min(Math.max(input.min, headerData[input.name] - Math.sign(e.deltaY) * 1), input.max)
+    showValue()
+  })
+
+  input.addEventListener('change', () => {
+    headerData[input.name] = input.valueAsNumber
+    showValue()
+  })
+
+  input.addEventListener('dblclick', () => {
+    headerData[input.name] = defaultValue
+    showValue()
+  })
+
+  showValue()
+}
+
 export const InstrumentEditor = {
   /**
    * @param {HTMLElement} parent
@@ -34,91 +60,25 @@ export const InstrumentEditor = {
     form.name.addEventListener('change', () => headerData.name = form.name.value)
 
     /* Volume */
-    const showVolume = () => {
-      form.volume.valueAsNumber = headerData.volume
-      form['volume-result'].value = displaydB(convertVolume(headerData.volume))
-    }
-    showVolume()
-
-    form.volume.addEventListener('wheel', (e) => {
-      headerData.volume = Math.min(Math.max(0, headerData.volume - Math.sign(e.deltaY) * 1), 100)
-      showVolume()
-    })
-
-    form.volume.addEventListener('change', () => {
-      headerData.volume = form.volume.valueAsNumber
-      showVolume()
-    })
-
-    form.volume.addEventListener('dblclick', () => {
-      headerData.volume = 50
-      showVolume()
+    activateSlider(form.volume, headerData, {
+      defaultValue: 50,
+      formatValue: (value) => displaydB(convertVolume(value))
     })
 
     /* Panning */
-    const showPanning = () => {
-      form.panning.valueAsNumber = headerData.panning
-      const displayValue = headerData.panning - 50
-      form['panning-result'].value = `${displayValue} (${displayValue < 0 ? 'left' : displayValue === 0 ? 'center' :  'right'})`
-    }
-    showPanning()
-
-    form.panning.addEventListener('wheel', (e) => {
-      headerData.panning = Math.min(Math.max(0, headerData.panning - Math.sign(e.deltaY) * 1), 100)
-      showPanning()
-    })
-
-    form.panning.addEventListener('change', () => {
-      headerData.panning = form.panning.valueAsNumber
-      showPanning()
-    })
-
-    form.panning.addEventListener('dblclick', () => {
-      headerData.panning = 50
-      showPanning()
+    activateSlider(form.panning, headerData, {
+      defaultValue: 50,
+      formatValue: (value) => {
+        const displayValue = value - 50
+        return `${displayValue} (${displayValue < 0 ? 'left' : displayValue === 0 ? 'center' : 'right'})`
+      }
     })
 
     /* Tune */
-    const showTune = () => {
-      form['tune-result'].value = form.tune.valueAsNumber = headerData.tune
-    }
-    showTune()
-
-    form.tune.addEventListener('wheel', (e) => {
-      headerData.tune = Math.min(Math.max(-24, headerData.tune - Math.sign(e.deltaY) * 1), 24)
-      showTune()
-    })
-
-    form.tune.addEventListener('change', () => {
-      headerData.tune = form.tune.valueAsNumber
-      showTune()
-    })
-
-    form.tune.addEventListener('dblclick', () => {
-      headerData.tune = 0
-      showTune()
-    })
+    activateSlider(form.tune, headerData)
 
     /* Finetune */
-    const showFinetune = () => {
-      form['finetune-result'].value = form.finetune.valueAsNumber = headerData.finetune
-    }
-    showFinetune()
-
-    form.finetune.addEventListener('wheel', (e) => {
-      headerData.finetune = Math.min(Math.max(-100, headerData.finetune - Math.sign(e.deltaY) * 1), 100)
-      showFinetune()
-    })
-
-    form.finetune.addEventListener('change', () => {
-      headerData.finetune = form.finetune.valueAsNumber
-      showFinetune()
-    })
-
-    form.finetune.addEventListener('dblclick', () => {
-      headerData.finetune = 0
-      showFinetune()
-    })
+    activateSlider(form.finetune, headerData)
 
     const mounted = Array.from(frag.children).map((el) => parent.appendChild(el))
 
